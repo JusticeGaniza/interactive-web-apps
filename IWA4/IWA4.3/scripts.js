d6d08921c41338dec069b384a1360db45691928f
Justice <rmirain@gmail.com>
2023-05-04 10:58:30 +0200

    finished IWA4.3

--- IWA4/IWA4.3/scripts.js
+++ IWA4/IWA4.3/scripts.js
@@ -1,33 +1,34 @@
-let date = 2050  //change semi colon to equals sign, changed const to let
-let status = 'student' //change semi colon to equals sign, changed const to let
-let count = 0 //changed const to let
+let date = 2050 //semi colon changed to an equal sign
+let status = 'student' //const changed to let, semi colon changed to an equal sign
+let count = 0 //const changed to let, semi colon changed to an equal sign
 
-if (date = 2050) {
-	console.log("January", 'New Year’s Day')      // closing bracket missing.
+if (date === 2050) { //added equal signs
+	console.log("January", 'New Year’s Day')
 	console.log("March", 'Human Rights Day')
-	// removed April date declaration added it to bottom line instead
-	console.log('April', 'Family Day')
-	console.log('April', 'Freedom Day')
+	date = 'April'
+	console.log(date, 'Family Day')
+	console.log(date, 'Freedom Day')
 	count = count + 4 //removed let from count
 
-	if (status = 'student') {
+	if (status === "student") { //added equal signs
 	  console.log('June', 'Youth Day')
-	  count = count + 1  //removed let from count
+	  count = count + 1 //removed let from count
   }
 
 	console.log('August', 'Women’s Day')
 	console.log('September', 'Heritage Day')
-	// removed December date declaration added it to bottom line instead
-	console.log('December', 'Day of Reconciliation')
+	date = 'December'
+	console.log(date, 'Day of Reconciliation')
 	count = count + 3 //removed let from count
 
-	if (status = 'parent') {
-	  console.log('December', 'Christmas Day') // replaced date with december string instead
-	  count = count + 1  //removed let from count
+	if (status === "parent") {
+	  console.log(date, 'Christmas Day')
+	  count = count + 1 //removed let from count
   }
 
-	console.log('December', 'Day of Goodwill') // replaced date with december string instead
-	count = count + 1  //removed let from count
+	console.log(date, 'Day of Goodwill')
+	count = count + 1 //removed let from count
+	date = '2050' //added date 2050
 }
 
 console.log('Your status is:', status)
